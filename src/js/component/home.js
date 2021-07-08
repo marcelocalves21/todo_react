9713c352d95a89f0629116a80976e801c1dab067
Marcelo C Alves <marcelocalves@live.com>
2021-07-08 01:23:53 +0000

    Todo list done, need to create an erase function and design

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 export function Home() {
+	const [list, setList] = useState([]);
+	const [todo, setTodo] = useState("");
+
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<h1>Todo</h1>
+			<input
+				className="m-3"
+				value={todo}
+				placeholder="What needs to be done?"
+				onChange={e => setTodo(e.target.value)}
+			/>
+			<button
+				className="btn btn-success"
+				onClick={() => {
+					setList([...list, todo]);
+					setTodo("");
+				}}>
+				add
+			</button>
+			{list.map((element, index) => (
+				<div key={index}>
+					{element}
+					<button>x</button>
+				</div>
+			))}
 		</div>
 	);
 }
